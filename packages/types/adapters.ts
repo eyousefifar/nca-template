ea86866e81947937f1112cbba8bc403563a76c01
eyousefifar <eyousefifar@gmail.com>
2020-10-28 20:43:23 +0330

    change types to snake case

--- packages/types/adapters.ts
+++ packages/types/adapters.ts
@@ -1,9 +1,9 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import {  types } from "cassandra-driver";
 
-export type TRow = types.Row;
-export type TLong = types.Long;
-export type TRequest = FastifyRequest;
+export type t_row = types.Row;
+export type t_long = types.Long;
+export type t_request = FastifyRequest;
 // http interfaces start
-export type TReply = FastifyReply;
+export type t_reply = FastifyReply;
 // http interfaces end
