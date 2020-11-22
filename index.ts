051c6066f78a4dab3430d2f16befa1b003a66275
eyousefifar <eyousefifar@gmail.com>
2020-11-22 14:06:44 +0330

    start servers here at main index

--- index.ts
+++ index.ts
@@ -1,11 +1,1 @@
-/* 
-    for monolith micro code
-    this file will be used to export interfaces that will allow
-    all micro services to run in one instance
-*/
-
-
-import {startServer } from './packages/interface'
-
-
-startServer();
+// start server(s) here
