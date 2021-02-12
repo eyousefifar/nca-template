e5113c8936ed11286c50a158ccfc39f1a7047bf4
eyousefifar <eyousefifar@gmail.com>
2021-02-12 20:28:22 +0330

    remove default types all exist at aba-utils

--- packages/types/adapters.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyRequest, FastifyReply } from "fastify";
-import {  types } from "cassandra-driver";
-
-export type t_row = types.Row;
-export type t_long = types.Long;
-export type t_request = FastifyRequest;
-// http interfaces start
-export type t_reply = FastifyReply;
-// http interfaces end
