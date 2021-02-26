2de96562c2022cc11e556caa36d9db0f0be07356
eyousefifar <eyousefifar@gmail.com>
2021-02-26 20:37:36 +0330

    export every interface by dafault

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,5 +1,5 @@
-// export {} from "./entities";
-// export {} from "./usecases";
-// export {} from "./controllers";
-// export {} from "./interfaces";
-// export {} from "./adapters"
+// export * from "./entities";
+// export * from "./usecases";
+// export * from "./controllers";
+// export * from "./interfaces";
+// export * from "./adapters"
